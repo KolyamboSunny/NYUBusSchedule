fce34f468e96ecdcd30cc0c02c15e42534ee40ce
Николай Санников <kolyambosunny@gmail.com>
2019-10-19 05:21:52 -0400

    Implemented showing list of known trips in Trips Activity

--- app/src/main/java/ru/nsunny/nyubustracker/TripsActivity.java
+++ app/src/main/java/ru/nsunny/nyubustracker/TripsActivity.java
@@ -3,7 +3,10 @@ package ru.nsunny.nyubustracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.ActionBar;
@@ -18,7 +21,7 @@ public class TripsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_route);
+        setContentView(R.layout.activity_trips);
 
         this.tripRepository = new DataRepository(getApplication());
 
@@ -36,7 +39,16 @@ public class TripsActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        List<Trip> currentTrips = tripRepository.getAllTrips();
+        List<Trip> allTrips = tripRepository.getAllTrips();
+        List<String> tripNames = new ArrayList<>();
+
+        for(Trip trip : allTrips){
+            tripNames.add(trip.getName());
+        }
+
+        ListView tripList = (ListView)findViewById(R.id.list_trips);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,tripNames);
+        tripList.setAdapter(adapter);
     }
 
     public void onAddRouteClick(View view){
